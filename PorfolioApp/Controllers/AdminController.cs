b03d8c88baa95f9aa2e64f130a510c7fba626470
erman konyar <er.konyar@gmail.com>
2023-01-04 01:21:15 +0300

    HelloWorld

---
+++ PorfolioApp/Controllers/AdminController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Project.Controllers
+{
+    public class AdminController : Controller
+    {
+        public PartialViewResult PartialSidebar()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialFooter()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialNavbar()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialHead()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialScript()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialNavigation()
+        {
+            return PartialView();
+        }
+    }
+}
--- PorfolioApp/Controllers/SkillController.cs
+++ PorfolioApp/Controllers/SkillController.cs
@@ -1,12 +1,36 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Core_Project.Controllers
 {
     public class SkillController : Controller
     {
+        SkillManager skillManager = new SkillManager(new EfSkillDal());
         public IActionResult Index()
         {
+            ViewBag.V1 = "Yetenek Listesi";
+            ViewBag.V2 = "Yetenekler";
+            ViewBag.V3 = "Yetenek Listesi";
+            var values = skillManager.GetList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult SkillAdd()
+        {
+            ViewBag.V1 = "Yetenek Ekleme";
+            ViewBag.V2 = "Yetenekler";
+            ViewBag.V3 = "Yetenek Ekleme";
+
             return View();
+        }
+        [HttpPost]
+        public IActionResult SkillAdd(Skill p)
+        {
+            skillManager.TAdd(p);
+            return RedirectToAction("Index");
         }
     }
 }
